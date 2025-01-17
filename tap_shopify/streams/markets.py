8c9cd4b82bbff82597ce770209eadf0bbaed1810
Renan Butkeraites <renan@hotglue.io>
2025-01-16 21:37:18 -0300

    Enhance markets stream with region details and update GraphQL query
    - Updated the gql_query to include market regions, enhancing the data structure returned for markets.
    - Added a new method, get_region_details, to fetch detailed information about market regions based on their IDs.
    - Implemented get_regions method to process and enrich market node data with region details.
    - This update improves the comprehensiveness of market data and enhances the overall functionality of the markets stream.

--- tap_shopify/streams/markets.py
+++ tap_shopify/streams/markets.py
@@ -1,3 +1,4 @@
+import copy
 from tap_shopify.context import Context
 from tap_shopify.streams.base import Stream
 import os
@@ -27,14 +28,54 @@ class Markets(Stream):
     replication_method = 'FULL_TABLE'
     replication_key = None
     results_per_page = 100
-    gql_query = "query tapShopify($first: Int, $after: String) { markets(first: $first, after: $after) { edges { cursor node { currencySettings { baseCurrency { currencyCode currencyName enabled rateUpdatedAt } localCurrencies } enabled handle id name primary webPresence { alternateLocales {locale marketWebPresences { id } name primary published } defaultLocale { locale marketWebPresences { id } name primary published } id rootUrls { locale url } subfolderSuffix } } }, pageInfo { hasNextPage } } }"
-
+    gql_query = "query tapShopify($first: Int, $after: String) { markets(first: $first, after: $after) { edges { cursor node { currencySettings { baseCurrency { currencyCode currencyName enabled rateUpdatedAt } localCurrencies } enabled handle id name primary regions(first: 250) { edges { node { id name } } } webPresence { alternateLocales {locale marketWebPresences { id } name primary published } defaultLocale { locale marketWebPresences { id } name primary published } id rootUrls { locale url } subfolderSuffix } } }, pageInfo { hasNextPage } } }"
     @shopify_error_handling
     def call_api_for_incoming_items(self):
         gql_client = shopify.GraphQL()
         with HiddenPrints():
             response = gql_client.execute(self.gql_query, dict(first=self.results_per_page))
         return json.loads(response)
+
+    @shopify_error_handling
+    def get_region_details(self, market_region_countries_ids):
+        region_query = """
+        query getRegionDetails($ids: [ID!]!) {
+            nodes(ids: $ids) {
+                id
+                ... on MarketRegionCountry {
+                code
+                name
+                }
+            }
+        }
+        """
+        
+        # Make API call
+        gql_client = shopify.GraphQL()
+        with HiddenPrints():
+            response = gql_client.execute(region_query, {'ids': market_region_countries_ids})
+        
+        region_data = json.loads(response)
+        
+        # Extract relevant data from response
+        if region_data.get('data', {}).get('nodes'):
+            details = region_data['data']['nodes']
+            return details
+        
+        LOGGER.warning(f"Could not fetch details for region ID: {market_region_countries_ids}")
+        return None
+
+    def get_regions(self, node):
+        final_regions = []
+        regions = node.get("regions")
+        edges = regions.get("edges")
+        market_region_countries_ids = []
+        for edge in edges:
+            edge_node = edge.get("node")
+            market_region_countries_ids.append(edge_node.get("id"))
+        new_node = copy.deepcopy(node)
+        new_node["regions"] = self.get_region_details(market_region_countries_ids)
+        return new_node
 
     def get_objects(self):
         incoming_item = self.call_api_for_incoming_items()
@@ -55,6 +96,7 @@ class Markets(Stream):
             return
         for edge in edges:
             node = edge.get("node")
+            node = self.get_regions(node)
             if node is not None:
                 yield node
             else:
